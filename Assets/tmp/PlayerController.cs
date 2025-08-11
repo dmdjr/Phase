a467aced44693195415f44f16949eb11819c5acf
Song <awesomeday322@gmail.com>
2025-08-11 16:17:43 +0900

    feat(player): movement & jump with cleaner controls

--- Assets/tmp/PlayerController.cs
+++ Assets/tmp/PlayerController.cs
@@ -5,95 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public Vector3 cameraTargetPosition;
+    public float jumpPower = 10f;
 
-    public Sprite player0Sprite; // 원래 스프라이트
-    public Sprite player1Sprite; // 반전된 스프라이트
+    Rigidbody2D Rigidbody2D;
 
-    private SpriteRenderer playerRenderer;
+    [Header("[GroundCheck]")]
+    public Transform groundCheck;
+    public float groundRadius = 0.15f;
+    public LayerMask groundLayer;
 
-    private Dictionary<GameObject, Color> originalColors = new Dictionary<GameObject, Color>();
-    private Dictionary<GameObject, Color> invertedColors = new Dictionary<GameObject, Color>();
-    private Color originalCameraColor;
-    private Color invertedCameraColor;
-
-    void Start()
+    bool isGrounded;
+    void Awake()
     {
-        playerRenderer = GetComponent<SpriteRenderer>();
-
-        // Map, Escape 태그 오브젝트 색 저장 및 역색 미리 계산
-        string[] targetTags = { "Map", "Escape" };
-        foreach (string tag in targetTags)
-        {
-            GameObject[] objects = GameObject.FindGameObjectsWithTag(tag);
-            foreach (GameObject obj in objects)
-            {
-                SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
-                if (sr != null)
-                {
-                    originalColors[obj] = sr.color;
-                    invertedColors[obj] = Invert(sr.color);
-                }
-            }
-        }
-
-        // 카메라 원래 배경색 저장 및 역색 계산
-        originalCameraColor = Camera.main.backgroundColor;
-        invertedCameraColor = Invert(originalCameraColor);
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        // 바닥 체크
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer) != null;
         // 이동 처리
-        Vector3 move = Vector3.zero;
-        if (Input.GetKey(KeyCode.UpArrow)) move += Vector3.up;
-        if (Input.GetKey(KeyCode.DownArrow)) move += Vector3.down;
-        if (Input.GetKey(KeyCode.LeftArrow)) move += Vector3.left;
-        if (Input.GetKey(KeyCode.RightArrow)) move += Vector3.right;
-        transform.position += move * moveSpeed * Time.deltaTime;
-
-        // 스페이스 홀드 중엔 미리 계산해둔 역색 + 스프라이트 전환 적용
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
-            foreach (var pair in invertedColors)
-            {
-                var sr = pair.Key.GetComponent<SpriteRenderer>();
-                if (sr != null) sr.color = pair.Value;
-            }
-            Camera.main.backgroundColor = invertedCameraColor;
-
-            if (playerRenderer.sprite != player1Sprite)
-                playerRenderer.sprite = player1Sprite;
+            Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x, jumpPower);
         }
-        else
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            foreach (var pair in originalColors)
-            {
-                var sr = pair.Key.GetComponent<SpriteRenderer>();
-                if (sr != null) sr.color = pair.Value;
-            }
-            Camera.main.backgroundColor = originalCameraColor;
-
-            if (playerRenderer.sprite != player0Sprite)
-                playerRenderer.sprite = player0Sprite;
+            transform.position += (Vector3)Vector2.left * moveSpeed * Time.deltaTime;
         }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += (Vector3)Vector2.right * moveSpeed * Time.deltaTime;
+        }
+
+
     }
 
-    // RGB 역색 계산
-    private Color Invert(Color c)
-    {
-        return new Color(1f - c.r, 1f - c.g, 1f - c.b, c.a);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Escape"))
-        {
-            Camera.main.transform.position = new Vector3(
-                cameraTargetPosition.x,
-                cameraTargetPosition.y,
-                Camera.main.transform.position.z
-            );
-        }
-    }
 }
