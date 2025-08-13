e29f696c65a54824db3d4c2b7ade36d815f3dc34
Song <awesomeday322@gmail.com>
2025-08-13 12:12:56 +0900

    feat: implement camera color inversion

--- Assets/@Scripts/Managers/InversionManager.cs
+++ Assets/@Scripts/Managers/InversionManager.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 
 public class InversionManager : MonoBehaviour
 {
+    [Header("카메라 설정")]
+    private Camera mainCamera;
+
     [Header("타일맵 오브젝트 설정")]
     public GameObject normalTilemap;
     public GameObject invertedTilemap;
 
-    private void Start()
-    {
-        normalTilemap.SetActive(true);
-        invertedTilemap.SetActive(false);
-    }
 
     [Header("일반 오브젝트 설정")]
     private List<InvertibleObject> allInvertibleObjects;
 
     private void Awake()
     {
+        mainCamera = Camera.main;
         allInvertibleObjects = new List<InvertibleObject>(FindObjectsByType<InvertibleObject>(FindObjectsSortMode.None));
+    }
+    private void Start()
+    {
+        mainCamera.backgroundColor = Color.black;
+
+        normalTilemap.SetActive(true);
+        invertedTilemap.SetActive(false);
     }
     private void Update()
     {
@@ -36,6 +42,8 @@ public class InversionManager : MonoBehaviour
     }
     private void SetAllObjectsInverted(bool isInverted)
     {
+
+        mainCamera.backgroundColor = isInverted ? Color.white : Color.black;
         normalTilemap.SetActive(!isInverted);
         invertedTilemap.SetActive(isInverted);
         foreach (InvertibleObject obj in allInvertibleObjects)
