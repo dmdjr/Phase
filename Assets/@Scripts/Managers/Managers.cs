0b96698047eb2afe6807b3d7154521cd583f96f8
나예진 <nyj500@konkuk.ac.kr>
2025-08-16 16:01:40 +0900

    upadte: test scene

--- Assets/@Scripts/Managers/Managers.cs
+++ Assets/@Scripts/Managers/Managers.cs
@@ -13,10 +13,12 @@ public class Managers : MonoBehaviour
     private ResourceManager _resource = new ResourceManager();
     private SoundManager _sound = new SoundManager();
     private UIManager _ui = new UIManager();
+    // private InversionManager _inversion = new InversionManager();
 
     public static ResourceManager Resource { get { return Instance?._resource; } }
     public static SoundManager Sound { get { return Instance?._sound; } }
     public static UIManager UI { get { return Instance?._ui; } }
+    // public static InversionManager Inversion { get { return Instance?._inversion; } }
 
     public static void Init()
     {
