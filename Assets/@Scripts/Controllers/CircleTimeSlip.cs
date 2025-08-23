16ce6e2fbbb529dd5c6f5565f5b8d08bac93885d
Song <awesomeday322@gmail.com>
2025-08-23 21:08:41 +0900

    feat: TimeSlip.cs & TimeEffected.cs 클래스 설계

--- Assets/@Scripts/Controllers/CircleTimeSlip.cs
+++ Assets/@Scripts/Controllers/CircleTimeSlip.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeSlip : MonoBehaviour
+public class CircleTimeSlip : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
         
     }
