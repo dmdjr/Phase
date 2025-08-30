5f180c0d6fb6769831269032cf5e9c92c956d407
나예진 <nyj500@konkuk.ac.kr>
2025-08-31 03:03:57 +0900

    fix: Enemy CooldownBar

--- Assets/@Scripts/Controllers/Objects/CooldownBar.cs
+++ Assets/@Scripts/Controllers/Objects/CooldownBar.cs
@@ -10,9 +10,14 @@ public class CooldownBar : MonoBehaviour
     public Enemy enemy;
 
     private float _t = 0; // active된 이후 흐른 시간
-    private float originScaleX = 2.9f;
+    private float minScaleX = 0.05f;
+    private float maxScaleX = 1.45f;
     private float originScaleY = 0.15f;
-    
+
+    void Awake()
+    {
+        enemy = gameObject.transform.parent.GetComponent<Enemy>();
+    }
     void OnEnable()
     {
         _t = 0f;      
@@ -25,8 +30,16 @@ public class CooldownBar : MonoBehaviour
 
     void Update()
     {
-        _t += Time.deltaTime;
-        float progress = Mathf.Clamp01( _t / enemy.shotInterval);
-        fillBar.localScale = new Vector3(progress * originScaleX, originScaleY, 0f);
+        if (enemy.isPlayerDetected)
+        {
+            _t += Time.deltaTime;
+            float progress = Mathf.Clamp01(_t / enemy.shotInterval);
+            fillBar.localScale = new Vector3(progress * maxScaleX, originScaleY, 0f);
+        }
+        else
+        {
+            fillBar.localScale = new Vector3(minScaleX, originScaleY, 0f);
+            ResetTimer();
+        }
     }
 }
