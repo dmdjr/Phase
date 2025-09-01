1d24518802f1a0f6551c6645fdd9e859c657044a
Song <awesomeday322@gmail.com>
2025-09-02 00:17:47 +0900

    feat: Stage5 연출 효과 추가

--- Assets/@Scripts/Controllers/SkillUnlockItem.cs
+++ Assets/@Scripts/Controllers/SkillUnlockItem.cs
@@ -9,6 +9,14 @@ public class SkillUnlockItem : MonoBehaviour
         // Player와 충돌하면
         if (collision.CompareTag("Player"))
         {
+            // 메인 카메라에서 CameraController 컴포넌트를 찾음
+            CameraController cameraController = Camera.main.GetComponent<CameraController>();
+            if (cameraController != null)
+            {
+                cameraController.RestoreCameraPosz();
+            }
+
+
             // 플레이어의 SkillController 컴포넌트를 찾아서 활성화(enable)시킴
             SkillController skill = collision.GetComponent<SkillController>();
             if (skill != null)
